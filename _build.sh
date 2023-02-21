c50011a9d270081375d48b59670bf6c29993436d
Akki Khare <admin@akki.io>
2023-02-20 20:06:37 -0500

    feat: upgrade to lumen 9

--- _build.sh
+++ _build.sh
@@ -1,2 +1,2 @@
-docker build -t gcr.io/akki-ca/html2pdf:1.2 -t gcr.io/akki-ca/html2pdf:latest . \
-    && docker push gcr.io/akki-ca/html2pdf:1.2 && docker push gcr.io/akki-ca/html2pdf:latest
+docker build -t gcr.io/akki-ca/html2pdf:1.3 -t gcr.io/akki-ca/html2pdf:latest . \
+    && docker push gcr.io/akki-ca/html2pdf:1.3 && docker push gcr.io/akki-ca/html2pdf:latest
